2f932d225e3cf9bb0633dafc30d4684a70ab57cd
Andrey Pavlov <yokinda@gmail.com>
2019-01-25 03:17:11 +0300

    Версионирование csv датой

--- parser/parser-banki-ru-eur.py
+++ parser/parser-banki-ru-eur.py
@@ -1,16 +1,17 @@
-from utils import get_html_seleniumm, get_exchangers_rates, get_banks_rates, save_csv 
+from datetime import datetime
+from utils import get_html_selenium, get_exchangers_rates, get_banks_rates, save_csv 
 
 URL_EUR = 'http://www.banki.ru/products/currency/cash/eur/moskva/#bank-rates'
 FILDS_EUR = ['dep_name', 'bank_name', 'eur_buy', 'eur_sell', 'update_time']
-FILE_EXCH_EUR = 'banki-ru-exchangers-eur.csv'
-FILE_BANKS_EUR = 'banki-ru-banks-eur.csv'
+FILE_EXCH_EUR = 'banki-ru-exchangers-eur_{}.csv'.format(datetime.now().strftime('%Y%m%d-%H%M%S'))
+FILE_BANKS_EUR = 'banki-ru-banks-eur_{}.csv'.format(datetime.now().strftime('%Y%m%d-%H%M%S'))
 
 def main():
-    html = get_html_seleniumm (URL_EUR)
+    html = get_html_selenium (URL_EUR)
 
     if html:
-        save_csv(get_exchangers_rates(html, FILDS_EUR, 'EUR'), FILE_EXCH_EUR, FILDS_EUR) #carrency need uppercase
-        save_csv(get_banks_rates(html, FILDS_EUR[1:], 'eur'), FILE_BANKS_EUR, FILDS_EUR[1:]) #carrency need lowercase
+        save_csv(get_exchangers_rates(html, FILDS_EUR, 'EUR'), FILE_EXCH_EUR, FILDS_EUR) #currency need uppercase
+        save_csv(get_banks_rates(html, FILDS_EUR[1:], 'eur'), FILE_BANKS_EUR, FILDS_EUR[1:]) #currency need lowercase
 
 if __name__ == '__main__':
     main()
--- parser/parser-banki-ru-usd.py
+++ parser/parser-banki-ru-usd.py
@@ -1,16 +1,17 @@
-from utils import get_html_seleniumm, get_exchangers_rates, get_banks_rates, save_csv 
+from datetime import datetime
+from utils import get_html_selenium, get_exchangers_rates, get_banks_rates, save_csv 
 
 URL_USD = 'http://www.banki.ru/products/currency/cash/usd/moskva/#bank-rates'
 FILDS_USD = ['dep_name', 'bank_name', 'usd_buy', 'usd_sell', 'update_time']
-FILE_EXCH_USD = 'banki-ru-exchangers-usd.csv'
-FILE_BANKS_USD = 'banki-ru-banks-usd.csv'
+FILE_EXCH_USD = 'banki-ru-exchangers-usd_{}.csv'.format(datetime.now().strftime('%Y%m%d-%H%M%S'))
+FILE_BANKS_USD = 'banki-ru-banks-usd_{}.csv'.format(datetime.now().strftime('%Y%m%d-%H%M%S'))
 
 def main():
-    html = get_html_seleniumm (URL_USD)
+    html = get_html_selenium (URL_USD)
 
     if html:
-        save_csv(get_exchangers_rates(html, FILDS_USD, 'USD'), FILE_EXCH_USD, FILDS_USD) #carrency need uppercase
-        save_csv(get_banks_rates(html, FILDS_USD[1:], 'usd'), FILE_BANKS_USD, FILDS_USD[1:]) #carrency need lowercase
+        save_csv(get_exchangers_rates(html, FILDS_USD, 'USD'), FILE_EXCH_USD, FILDS_USD) #currency need uppercase
+        save_csv(get_banks_rates(html, FILDS_USD[1:], 'usd'), FILE_BANKS_USD, FILDS_USD[1:]) #currency need lowercase
 
 if __name__ == '__main__':
     main()
