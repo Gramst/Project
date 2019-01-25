031f8d6d043cc22debf4d15c159f43d6953293b3
Andrey Pavlov <yokinda@gmail.com>
2019-01-25 03:15:06 +0300

    Версионирование csv датой, запись в отдельную папку

--- parser/utils.py
+++ parser/utils.py
@@ -1,9 +1,10 @@
 import csv
 #import requests
 from bs4 import BeautifulSoup
+import os
 from selenium import webdriver
 
-def get_html_seleniumm(url):
+def get_html_selenium(url):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     driver = webdriver.Chrome(chrome_options=options)
@@ -12,7 +13,7 @@ def get_html_seleniumm(url):
     driver.close()
     return generated_html
 
-def get_exchangers_rates(html, fields, carrency):
+def get_exchangers_rates(html, fields, currency):
     soup = BeautifulSoup(html, 'html.parser')
     rows_exchagers = soup.findAll('div', class_='table-flex__row item calculator-hover-icon__container')
     exchangers_table = []
@@ -21,14 +22,14 @@ def get_exchangers_rates(html, fields, carrency):
         exchangers_table.append({
             'dep_name' : row.find('a', class_='font-bold').text,
             'bank_name' : row.find('a', class_='font-size-default').text,
-            fields[2] : row.find('div', attrs={'data-currencies-code' : carrency})['data-currencies-rate-buy'],
-            fields[3] : row.findAll('div', attrs={'data-currencies-code' : carrency})[1]['data-currencies-rate-sell'],
+            fields[2] : row.find('div', attrs={'data-currencies-code' : currency})['data-currencies-rate-buy'],
+            fields[3] : row.findAll('div', attrs={'data-currencies-code' : currency})[1]['data-currencies-rate-sell'],
             'update_time' : row.find('div', class_='font-size-small color-gray-gray').text[:-1]
         })
 
     return exchangers_table
 
-def get_banks_rates(html, fields, carrency):
+def get_banks_rates(html, fields, currency):
     soup = BeautifulSoup(html, 'html.parser')
     rows_banks = soup.findAll('tr', class_='calculator-hover-icon__container exchange-calculator-rates ')
     banks_table = []
@@ -36,15 +37,17 @@ def get_banks_rates(html, fields, carrency):
     for row in rows_banks:
         banks_table.append({
             'bank_name' : row.find('a', class_='font-bold').text,
-            fields[1] : row.find('td', attrs={'data-currencies-code' : carrency})['data-currencies-rate-buy'],
-            fields[2] : row.findAll('td', attrs={'data-currencies-code' : carrency})[1]['data-currencies-rate-sell'],
+            fields[1] : row.find('td', attrs={'data-currencies-code' : currency})['data-currencies-rate-buy'],
+            fields[2] : row.findAll('td', attrs={'data-currencies-code' : currency})[1]['data-currencies-rate-sell'],
             'update_time' : row.find('span', class_='font-size-default').text
         })
 
     return banks_table
 
 def save_csv(result_table, save_file, fields):
-    with open(save_file, "w", encoding="utf8", newline='') as csvf:
+    csv_dir = 'csv_files'
+    os.makedirs(csv_dir, exist_ok = True)
+    with open(os.path.join(csv_dir, save_file), "w", encoding="utf8", newline='') as csvf:
         writer = csv.DictWriter(csvf, fields, delimiter=';')
         writer.writeheader()
         for row in result_table:
