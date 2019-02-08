dd88488a0d018ee0883863e31c095cb1686fecde
Andrey Pavlov <yokinda@gmail.com>
2019-02-08 14:33:29 +0300

    Обновил парсер на новую страницу - получаем сразу две пары валют одновременно Добавил возможность обработки по городам

--- parser/parser-banki-ru-usd.py
+++
@@ -1,7 +0,0 @@
-from utils import get_banki_ru_rates
-
-def main():
-    get_banki_ru_rates('usd')
-
-if __name__ == '__main__':
-    main()
--- parser/utils.py
+++ parser/utils.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-import settings
+from settings import BANKS_TR, CSV_DIR, EXCH_DIV, FIELDS
 
 def get_html_selenium(url):
     options = webdriver.ChromeOptions()
@@ -16,36 +16,36 @@ def get_html_selenium(url):
     driver.close()
     return generated_html
 
-def get_exchangers_rates(html, fields, currency):
+def get_usd_eur_rates(html, sity_ru):
     soup = BeautifulSoup(html, 'html.parser')
-    rows_exchagers = soup.findAll('div', class_=settings.EXCH_DIV)
-    exchangers_table = []
+    rows_exchagers = soup.findAll('div', class_=EXCH_DIV)
+    currency_table = []
 
     for row in rows_exchagers:
-        exchangers_table.append({
+        currency_table.append({
             'dep_name' : row.find('a', class_='font-bold').text,
             'bank_name' : row.find('a', class_='font-size-default').text,
-            fields[2] : row.find('div', attrs={'data-currencies-code' : currency})['data-currencies-rate-buy'],
-            fields[3] : row.findAll('div', attrs={'data-currencies-code' : currency})[1]['data-currencies-rate-sell'],
+            'usd_rur' : row.find('div', attrs={'data-currencies-code' : 'USD'})['data-currencies-rate-buy'],
+            'rur_usd' : row.findAll('div', attrs={'data-currencies-code' : 'USD'})[1]['data-currencies-rate-sell'],
+            'eur_rur' : row.find('div', attrs={'data-currencies-code' : 'EUR'})['data-currencies-rate-buy'],
+            'rur_eur' : row.findAll('div', attrs={'data-currencies-code' : 'EUR'})[1]['data-currencies-rate-sell'],
             'update_time' : row.find('div', class_='font-size-small color-gray-gray').text[:-1]
         })
 
-    return exchangers_table
-
-def get_banks_rates(html, fields, currency):
-    soup = BeautifulSoup(html, 'html.parser')
-    rows_banks = soup.findAll('tr', class_=settings.BANKS_TR)
-    banks_table = []
+    rows_banks = soup.findAll('tr', class_=BANKS_TR)
 
     for row in rows_banks:
-        banks_table.append({
+        currency_table.append({
+            'dep_name' : '{} - {}'.format(row.find('a', class_='font-bold').text, sity_ru),            
             'bank_name' : row.find('a', class_='font-bold').text,
-            fields[1] : row.find('td', attrs={'data-currencies-code' : currency})['data-currencies-rate-buy'],
-            fields[2] : row.findAll('td', attrs={'data-currencies-code' : currency})[1]['data-currencies-rate-sell'],
+            'usd_rur' : row.find('td', attrs={'data-currencies-code' : 'usd'})['data-currencies-rate-buy'],
+            'rur_usd' : row.findAll('td', attrs={'data-currencies-code' : 'usd'})[1]['data-currencies-rate-sell'],
+            'eur_rur' : row.find('td', attrs={'data-currencies-code' : 'eur'})['data-currencies-rate-buy'],
+            'rur_eur' : row.findAll('td', attrs={'data-currencies-code' : 'eur'})[1]['data-currencies-rate-sell'],
             'update_time' : row.find('span', class_='font-size-default').text
         })
 
-    return banks_table
+    return currency_table
 
 def save_csv(result_table, save_file, fields, csv_dir):
     os.makedirs(csv_dir, exist_ok = True)
@@ -55,15 +55,11 @@ def save_csv(result_table, save_file, fields, csv_dir):
         for row in result_table:
             writer.writerow(row)
 
-def get_banki_ru_rates(currency):
-    url = 'http://www.banki.ru/products/currency/cash/{}/moskva/#bank-rates'.format(currency)
-    fields = ['dep_name', 'bank_name', '{}_buy'.format(currency), '{}_sell'.format(currency), 'update_time']
-    file_exchangers = 'banki-ru-exchangers-{}_{}.csv'.format(currency, datetime.now().strftime('%Y%m%d-%H%M%S'))
-    file_banks = 'banki-ru-banks-{}_{}.csv'.format(currency, datetime.now().strftime('%Y%m%d-%H%M%S'))
+def get_banki_ru_rates(sity, sity_ru):
+    url = 'https://www.banki.ru/products/currency/cash/{}/#bank-rates'.format(sity)
+    file_rates = 'banki-ru-rates-{}_{}.csv'.format(sity, datetime.now().strftime('%Y%m%d-%H%M%S'))
 
     html = get_html_selenium (url)
 
     if html:
-        #for exchangers currency need uppercase
-        save_csv(get_exchangers_rates(html, fields, currency.upper()), file_exchangers, fields, settings.CSV_DIR)
-        save_csv(get_banks_rates(html, fields[1:], currency), file_banks, fields[1:], settings.CSV_DIR)
+        save_csv(get_usd_eur_rates(html, sity_ru), file_rates, FIELDS, CSV_DIR)
